dd50f8b0380f47fe84abd4f2dc2ccac88d06f928
aconsilvio <annabel@students.olin.edu>
2015-03-04 15:16:35 -0500

    code for hannah

--- food-world-cup/demo.py
+++ food-world-cup/demo.py
@@ -4,15 +4,6 @@ import csv
 import numpy as np
 import string
 
-def manage_data(filename):	
-	empty = []
-	with open(filename,"rU") as csvfile:
-		spamreader = csv.reader(csvfile,  quotechar = '|')
-		for row in spamreader:
-			lines = ', '.join(row)
-			lines = lines.split(",")
-			empty.append(lines)
-		return empty[:3]
 
 def get_numpy_array(csv_file):                                                                                                                                             
 	"""returns a numpy array built from the csv file. To stop numpy from crapping itself, uneven rows and columns are padded with empty strings so that all rows and            columns have the same number of elements"""                                                                                                                                 
@@ -20,9 +11,42 @@ def get_numpy_array(csv_file):
 	f = open(csv_file, "rU")                                                                                                                                                     
 	for row in csv.reader(f):                                                                                                                                              
 		data.append(row)                                                                                                                                                    
-	length = len(sorted(data,key=len, reverse=True)[0])                                                                                                                     
-	return np.array([x+['']*(length-len(x)) for x in data])  
+	length = len(sorted(data,key=len, reverse=True)[0])
+	return np.array([x+[]*(length-len(x)) for x in data])  
 
+def fix_data(filename):
+	a = get_numpy_array(filename)
+	remove = ["N/A"]
+	for element in a:
+		for index in range(len(element)):
+			if element[index] in remove:
+				element[index] = 0
+				print type(element[index])
+	return a
+
+
+def make_int(filename):
+	number = ['1','2','3','4','5']
+	l = fix_data(filename)
+	for element in l:
+		for index in range(len(element)):
+			if element[index] in number:
+				a = int(element[index])
+				b = a.astype(np.float)
+				a = b
+				element[index] = a
+				print type(element[index])
+	print l[1]
+
+	#for j in range(l.shape[1]):
+	#	for i in range(l.shape[0]):
+	#		if (l[i,j].isdigit()==True and len(l[i,j])==1):
+	#			np.sum(l[i,j])
+				#results[i] = results[i] + int(l[i,j])
+	#print results[4]
+
+	#print dataset.shape
+	#for i in range(len(dataset)) # width and then height
 
 def make_lists(filename):
 	l = []
@@ -30,20 +54,6 @@ def make_lists(filename):
 	for line in data:
 		l.append(line)
 	return l
-
-def find_ranking(filename):
-	number = ['1','2','3','4','5']
-	l = get_numpy_array(filename)
-	results = np.zeros((l.shape[1],1))
-	for i in range(l.shape[0]):
-		for j in range(l.shape[1]):
-			if l[i,j] in number:
-				results[i] = results[i] + int(l[i,j])
-	print results
-
-	#print dataset.shape
-	#for i in range(len(dataset)) # width and then height
-
 
 def make_dictionary(filename):
 	d = {}
@@ -63,8 +73,19 @@ def rank_country(filename):
 		for index in range(3,42):
 			print value
 
+def manage_data(filename):	
+	empty = []
+	with open(filename,"rU") as csvfile:
+		spamreader = csv.reader(csvfile,  quotechar = '|')
+		for row in spamreader:
+			lines = ', '.join(row)
+			lines = lines.split(",")
+			empty.append(lines)
+		return empty[:3]
 
-find_ranking("food-world-cup-data.csv")
+
+#fix_data("food-world-cup-data.csv")
+make_int("food-world-cup-data.csv")
 #manage_data("food-world-cup-data.csv")
 #rank_country("food-world-cup-data.csv")
 #make_dictionary("food-world-cup-data.csv")
