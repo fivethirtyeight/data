48d4b1210d3ee2c0f59b92d9bd8f844e4ae34836
Adam Obeng <github@binaryeagle.com>
2016-08-04 16:28:16 -0400

    Fix unit test
    Signed-off-by: Adam Obeng <github@binaryeagle.com>

--- repeated-phrases-gop/test_robopol2.py
+++ repeated-phrases-gop/test_robopol2.py
@@ -5,16 +5,16 @@ import robopol2
 class TestRobopol(unittest.TestCase):
     def test_ngram_freqs(self):
         corpus = 'the quick brown fox jumps over the lazy dog'.split()
-        self.assertEqual(
+        print(robopol2.ngram_freqs(corpus, 1))
+        self.assertDictEqual(
             robopol2.ngram_freqs(corpus, 1),
             {
-                ('the',): 1,
+                ('the',): 2,
                 ('quick',): 1,
                 ('brown',): 1,
                 ('fox',): 1,
                 ('jumps',): 1,
                 ('over',): 1,
-                ('the',): 1,
                 ('lazy',): 1,
                 ('dog',): 1,
             }
